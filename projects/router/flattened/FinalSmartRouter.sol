986ba17e5f74298b0f730479ba94ff7cedc460ac
Muhammad Ahmad <162286232+AhmadGill7@users.noreply.github.com>
2025-07-21 02:34:37 +0500

    flattened contracts

---
+++ projects/router/flattened/FinalSmartRouter.sol
@@ -0,0 +1,4 @@
+yarn run v1.22.22
+$ D:\Ahmad\Archive\KaspaFInanceDex\pancake-v3-contracts\node_modules\.bin\hardhat flatten contracts/SmartRouter.sol
+
+info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
